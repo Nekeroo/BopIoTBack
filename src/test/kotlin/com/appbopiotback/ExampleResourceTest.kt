6b5a8839a84d936ed4b417e42d76bba3c9f716b1
Nekeroo <m.grattardpro@gmail.com>
2024-11-18 14:46:50 +0100

    Chore : Ajout TU

--- src/test/kotlin/com/appbopiotback/ExampleResourceTest.kt
+++ src/test/kotlin/com/appbopiotback/ExampleResourceTest.kt
@@ -2,19 +2,28 @@ package com.appbopiotback
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.jupiter.api.Test
+import jakarta.ws.rs.core.MediaType
 
 @QuarkusTest
 class ExampleResourceTest {
 
     @Test
     fun testHelloEndpoint() {
-        given()
-          .`when`().get("/hello/mqtt")
-          .then()
-             .statusCode(200)
-             .body(`is`("Hello from Quarkus REST"))
-    }
+        val requestBody = """
+            {
+                "message": "Commande traitée"
+            }
+        """.trimIndent()
 
+        given()
+            .contentType(MediaType.APPLICATION_JSON) // Spécifie que le contenu est du JSON
+            .body(requestBody) // Corps de la requête JSON
+            .`when`()
+            .post("/hello/mqtt") // Appelle le POST à /hello/mqtt
+            .then()
+            .statusCode(200) // Vérifie que le code HTTP est 200
+            .body(containsString("Message envoyé")) // Vérifie que la réponse contient "Message envoyé"
+    }
 }
